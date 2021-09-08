dd4dfd7820c98f674b23a1ada580383cf23ff115
vismayatk002 <vismayatk002@gmail.com>
2021-09-08 20:50:33 +0530

    Done with 2nd smallest and 2nd largest without sorting array

--- 2ndLargestSmallest.js
+++ 2ndLargestSmallest.js
@@ -3,4 +3,31 @@ for(let i=0; i<10; i++){
     let number = Math.floor(Math.random() * 900) + 100;
     randomArr.push(number);
 }
-console.log(randomArr);
+let min;
+let max;
+let secondMin;
+let secondMax;
+
+for (let i = 0; i < 10; i++ ){
+
+    if(i == 0){
+        min = randomArr[i];
+        max = randomArr[i];
+    }
+    if(randomArr[i] < min){
+        secondMin = min;
+        min = randomArr[i];
+    }
+    else if(randomArr[i] < secondMin){
+        secondMin = randomArr[i];
+    }
+    if(randomArr[i] > max){
+        secondMax = max;
+        max = randomArr[i];
+    }
+    else if(randomArr[i] > secondMax){
+        secondMax = randomArr[i];
+    }
+}
+console.log('Second Smallest Number : ' + secondMin);
+console.log('Second Largest Number : ' + secondMax);
